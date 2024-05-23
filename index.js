48c5241f2f2243c44faece73cba1dc864b70742e
Edi092410 <130530620+Edi092410@users.noreply.github.com>
2024-05-23 20:22:20 +0800

    05-23

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var lis = document.querySelectorAll(".type-bottom li");
-//   lis.forEach(function (li) {
-//     li.addEventListener("click", function () {
-//       if (this.style.backgroundColor != "#93C759")
-//         this.style.backgroundColor = "#93C759";
-//       else this.style.backgroundColor = "red";
-//     });
-//   });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  var lis = document.querySelectorAll(".type-bottom li");
+  lis.forEach(function (li) {
+    li.addEventListener("click", function () {
+      if (this.style.backgroundColor != "#93C759")
+        this.style.backgroundColor = "#93C759";
+      else this.style.backgroundColor = "red";
+    });
+  });
+});
 
 //jsonbin
 
