a76d35cc508e4815a78c9ffb92d92025b19ce8c8
erkhemq <erkhemkhuu123@gmail.com>
2024-05-26 02:55:41 +0800

    auth added

---
+++ auth.js
@@ -0,0 +1,18 @@
+// auth.js
+
+function checkAuthentication() {
+  fetch('http://localhost:4000/api/users/authenticated', {
+    credentials: 'include'
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (!data.authenticated) {
+      window.location.href = 'https://erkhemq.github.io/cookly/signin.html';
+    }
+  })
+  .catch(error => {
+    console.error('Error checking authentication status:', error);
+  });
+}
+  
+checkAuthentication();
